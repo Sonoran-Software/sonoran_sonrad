8f3a90cf3c169b2c118c9ba006102138bccd2f93
Lansing <me@lansing.io>
2021-10-22 15:46:36 -0400

    Initial Commit

---
+++ sonrad/README_sonrad.md
@@ -0,0 +1,12 @@
+# Sonoran CAD - Sonoran Radio Plugin
+Sonoran Radio Plugin integrates Sonoran Radio with your CAD Installation
+
+## Installation
+
+All plugins follow a similar installation process. For more information, [click here to see our guides.](https://info.sonorancad.com/integration-plugins/integration-plugins)
+
+## Developing
+
+Developing a plugin for the Sonoran Lua Framework is rather simple but requires you to have existing knowledge of how global and local functions and lua objects work. There is also a lot of logic built into the core framework in order to facilitate easy updating which requires particular naming and folder structure.
+
+If you have any questions please reach out to our development community on [Discord](https://discord.sonoransoftware.com). See #request-role channel for details on our development community interaction.
---
+++ sonrad/sv_sonrad.lua
@@ -0,0 +1,87 @@
+--[[
+    Sonaran CAD Plugins
+
+    Plugin Name: sonrad
+    Creator: Sonoran Software Systems
+    Description: Sonoran Radio integration plugin
+
+    Put all server-side logic in this file.
+]]
+
+CreateThread(function() Config.LoadPlugin("sonrad", function(pluginConfig)
+
+    if pluginConfig.enabled then
+
+        -- Call UUID generation
+        local random = math.random
+        local function uuid()
+            math.randomseed(os.time())
+            local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+            return string.gsub(template, '[xy]', function (c)
+                local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
+                return string.format('%x', v)
+            end)
+        end
+
+        RegisterNetEvent("SonoranCAD::sonrad:RadioPanic")
+        AddEventHandler("SonoranCAD::sonrad:RadioPanic", function()
+            sendRadioPanic(source)
+        end)
+
+        RegisterNetEvent("SonoranCAD::sonrad:GetUnitInfo")
+        AddEventHandler("SonoranCAD::sonrad:GetUnitInfo", function()
+            local unit = GetUnitByPlayerId(source)
+            if unit ~= nil then print(json.encode(unit)) end
+            TriggerClientEvent("SonoranCAD::sonrad:RecvUnitInfo", source, unit)
+        end)
+
+        function sendRadioPanic(source)
+            -- Determine identifier
+            local source = tostring(source)
+            local identifier = GetIdentifiers(source)[Config.primaryIdentifier]
+
+            -- Process panic POST request
+            if pluginConfig.addPanicCall then
+                -- Process Unit
+                local unit = GetUnitByPlayerId(source)
+                if not unit then
+                    debugLog("Caller is not a unit, ignoring. id: " .. source .. " unit: " .. json.encode(unit))
+                    return
+                end
+                -- Process Postal
+                local postal = ""
+                if isPluginLoaded("postals") and PostalsCache[source] ~= nil then
+                    postal = PostalsCache[source]
+                else
+                    debugLog("postal is nil?!")
+                end
+                local data = {
+                    ['serverId'] = Config.serverId,
+                    ['isEmergency'] = true,
+                    ['caller'] = unit.data.name,
+                    ['location'] = unit.location,
+                    ['description'] = ("Unit %s has pressed their panic button"):format(unit.data.unitNum),
+                    ['metaData'] = {
+                        ['callerPlayerId'] = source,
+                        ['callerApiId'] = GetIdentifiers(source)[Config.primaryIdentifier],
+                        ['uuid'] = uuid(),
+                        ['silentAlert'] = false,
+                        ['useCallLocation'] = false,
+                        ['callPostal'] = postal
+                    }
+                }
+                if LocationCache[source] ~= nil then
+                    data['metaData']['callLocationx'] = LocationCache[source].position.x
+                    data['metaData']['callLocationy'] = LocationCache[source].position.y
+                    data['metaData']['callLocationz'] = LocationCache[source].position.z
+                else
+                    debugLog("Warning: location cache was nil, not sending position")
+                end
+                debugLog(("perform panic request %s"):format(json.encode(data)))
+                performApiRequest({data}, 'CALL_911', function(resp) debugLog(resp) end)
+            end
+            performApiRequest({{['isPanic'] = true, ['apiId'] = identifier}}, 'UNIT_PANIC', function() end)
+        end
+    end
+
+end) end)
