08ba68c1a44f24500b1ea8f0e79a5675b916be76
Nathan C <me@nathanc.tech>
2022-06-02 21:54:12 -0400

    fix: don't use blip feature < PRO

--- sonrad/sv_sonrad.lua
+++ sonrad/sv_sonrad.lua
@@ -16,211 +16,217 @@ CreateThread(function() Config.LoadPlugin("sonrad", function(pluginConfig)
         local UnitCache = {}
         local TowerCache = {}
 
-        -- Register Api Types
-        registerApiType("ADD_BLIP", "emergency")
-        registerApiType("MODIFY_BLIP", "emergency")
-        registerApiType("REMOVE_BLIP", "emergency")
-        registerApiType("GET_BLIPS", "emergency")
         
-        BlipMan = {
-            addBlip = function(coords, radius, colorHex, subType, toolTip, icon, dataTable, cb)
-                local data = {{
-                    ["serverId"] = GetConvar("sonoran_serverId", 1),
-                    ["blip"] = {
-                        ["id"] = -1,
-                        ["subType"] = subType,
-                        ["coordinates"] = {
-                            ["x"] = coords.x,
-                            ["y"] = coords.y
-                        },
-                        ["radius"] = radius,
-                        ["icon"] = icon,
-                        ["color"] = colorHex,
-                        ["tooltip"] = toolTip,
-                        ["data"] = dataTable
-                    }
-                }}
-                performApiRequest(data, "ADD_BLIP", function(res)
-                    if cb ~= nil then
-                        cb(res)
-                    end
-                end)
-            end,
-    
-            addBlips = function(blips, cb)
-                performApiRequest(blips, "ADD_BLIP", function(res)
-                    if cb ~= nil then
-                        cb(res)
-                    end
-                end)
-            end,
-    
-            removeBlip = function(ids, cb)
-                performApiRequest({{
-                    ["ids"] = ids
-                }}, "REMOVE_BLIP", function(res)
-                    if cb ~= nil then
-                        cb(res)
-                    end
-                end)
-            end,
-    
-            modifyBlips = function(dataTable, cb)
-                performApiRequest(dataTable, "MODIFY_BLIP", function(res)
-                    if cb ~= nil then
-                        cb(res)
-                    end
-                end)
-            end,
-
-            getBlips = function(cb)
-                local data = {{
-                    ["serverId"] = GetConvar("sonoran_serverId", 1)
-                }}
-                performApiRequest(data, "GET_BLIPS", function(res)
-                    if cb ~= nil then
-                        cb(res)
-                    end
-                end)
-            end,
-    
-            removeWithSubtype = function(subType, cb)
-                BlipMan.getBlips(function(res)
-                    local dres = json.decode(res)
-                    local ids = {}
-                    for _, v in ipairs(dres) do
-                        if v.subType == subType then
-                            table.insert(ids, #ids + 1, v.id)
-                        end
-                    end
-                    BlipMan.removeBlip(ids, cb)
-                end)
-            end,
-        }
-
-        function GetTower(coords)
-            for i = 1, #TowerCache do
-                if TowerCache[i].PropPosition == coords then
-                    return TowerCache[i], i
-                end
-            end
-            return nil, nil
-        end
-        function GetTowerFromId(id)
-            for _, t in ipairs(TowerCache) do
-                if t.Id == id then
-                    return t
-                end
-            end
-        end
-        function GetTowerCapacity(tower)
-            if #tower.DishStatus < 1 then
-                return 1.0
-            end
-        
-            local n = 0.0
-            for i = 1, #tower.DishStatus do
-                if tower.DishStatus[i] == 'alive' then
-                    n = n + 1.0
-                end
-            end
-            return n / #tower.DishStatus
-        end
-
-        RegisterNetEvent("SonoranCAD::sonrad:SyncTowers")
-        AddEventHandler("SonoranCAD::sonrad:SyncTowers", function(Towers)
-            BlipMan.removeWithSubtype("repeater", function(res)
-                debugLog(res)
-
-                TowerCache = Towers
-
-                local BlipQueue = {}
-
-                debugLog(json.encode(TowerCache))
-                for _,t in ipairs(TowerCache) do
-
-                    local CurrentBlip = {
+        if Config.apiVersion > 3 then
+            -- Register Api Types
+            registerApiType("ADD_BLIP", "emergency")
+            registerApiType("MODIFY_BLIP", "emergency")
+            registerApiType("REMOVE_BLIP", "emergency")
+            registerApiType("GET_BLIPS", "emergency")
+            
+            BlipMan = {
+                addBlip = function(coords, radius, colorHex, subType, toolTip, icon, dataTable, cb)
+                    local data = {{
                         ["serverId"] = GetConvar("sonoran_serverId", 1),
                         ["blip"] = {
                             ["id"] = -1,
-                            ["subType"] = "repeater",
+                            ["subType"] = subType,
                             ["coordinates"] = {
-                                ["x"] = t.PropPosition.x,
-                                ["y"] = t.PropPosition.y
+                                ["x"] = coords.x,
+                                ["y"] = coords.y
                             },
-                            ["radius"] = t.Range * 0.7937,
-                            ["icon"] = "https://sonoransoftware.com/assets/images/icons/email/radio.png",
-                            ["color"] = "#00a6ff",
-                            ["tooltip"] =  "Radio Tower",
-                            ["data"] = {
-                                {
-                                    ["title"] = "Health",
-                                    ["text"] = "HEALTHY",
+                            ["radius"] = radius,
+                            ["icon"] = icon,
+                            ["color"] = colorHex,
+                            ["tooltip"] = toolTip,
+                            ["data"] = dataTable
+                        }
+                    }}
+                    
+                    performApiRequest(data, "ADD_BLIP", function(res)
+                        if cb ~= nil then
+                            cb(res)
+                        end
+                    end)
+                end,
+        
+                addBlips = function(blips, cb)
+                    performApiRequest(blips, "ADD_BLIP", function(res)
+                        if cb ~= nil then
+                            cb(res)
+                        end
+                    end)
+                end,
+        
+                removeBlip = function(ids, cb)
+                    performApiRequest({{
+                        ["ids"] = ids
+                    }}, "REMOVE_BLIP", function(res)
+                        if cb ~= nil then
+                            cb(res)
+                        end
+                    end)
+                end,
+        
+                modifyBlips = function(dataTable, cb)
+                    performApiRequest(dataTable, "MODIFY_BLIP", function(res)
+                        if cb ~= nil then
+                            cb(res)
+                        end
+                    end)
+                end,
+
+                getBlips = function(cb)
+                    local data = {{
+                        ["serverId"] = GetConvar("sonoran_serverId", 1)
+                    }}
+                    performApiRequest(data, "GET_BLIPS", function(res)
+                        if cb ~= nil then
+                            cb(res)
+                        end
+                    end)
+                end,
+        
+                removeWithSubtype = function(subType, cb)
+                    BlipMan.getBlips(function(res)
+                        local dres = json.decode(res)
+                        local ids = {}
+                        for _, v in ipairs(dres) do
+                            if v.subType == subType then
+                                table.insert(ids, #ids + 1, v.id)
+                            end
+                        end
+                        BlipMan.removeBlip(ids, cb)
+                    end)
+                end,
+            }
+
+            function GetTower(coords)
+                for i = 1, #TowerCache do
+                    if TowerCache[i].PropPosition == coords then
+                        return TowerCache[i], i
+                    end
+                end
+                return nil, nil
+            end
+            function GetTowerFromId(id)
+                for _, t in ipairs(TowerCache) do
+                    if t.Id == id then
+                        return t
+                    end
+                end
+            end
+            function GetTowerCapacity(tower)
+                if #tower.DishStatus < 1 then
+                    return 1.0
+                end
+            
+                local n = 0.0
+                for i = 1, #tower.DishStatus do
+                    if tower.DishStatus[i] == 'alive' then
+                        n = n + 1.0
+                    end
+                end
+                return n / #tower.DishStatus
+            end
+
+            RegisterNetEvent("SonoranCAD::sonrad:SyncTowers")
+            AddEventHandler("SonoranCAD::sonrad:SyncTowers", function(Towers)
+                BlipMan.removeWithSubtype("repeater", function(res)
+                    debugLog(res)
+
+                    TowerCache = Towers
+
+                    local BlipQueue = {}
+
+                    debugLog(json.encode(TowerCache))
+                    for _,t in ipairs(TowerCache) do
+
+                        local CurrentBlip = {
+                            ["serverId"] = GetConvar("sonoran_serverId", 1),
+                            ["blip"] = {
+                                ["id"] = -1,
+                                ["subType"] = "repeater",
+                                ["coordinates"] = {
+                                    ["x"] = t.PropPosition.x,
+                                    ["y"] = t.PropPosition.y
+                                },
+                                ["radius"] = t.Range * 0.7937,
+                                ["icon"] = "https://sonoransoftware.com/assets/images/icons/email/radio.png",
+                                ["color"] = "#00a6ff",
+                                ["tooltip"] =  "Radio Tower",
+                                ["data"] = {
+                                    {
+                                        ["title"] = "Health",
+                                        ["text"] = "HEALTHY",
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    table.insert(BlipQueue, #BlipQueue + 1, CurrentBlip)
-                end
-
-                BlipMan.addBlips(BlipQueue, function(res)
-                    local blips = json.decode(res)
-                    for i=1, #TowerCache do
-                        TowerCache[i].BlipID = blips[i].id
+                        table.insert(BlipQueue, #BlipQueue + 1, CurrentBlip)
                     end
-                    debugLog("Tower Cache:" .. json.encode(TowerCache))
+
+                    BlipMan.addBlips(BlipQueue, function(res)
+                        local blips = json.decode(res)
+                        for i=1, #TowerCache do
+                            TowerCache[i].BlipID = blips[i].id
+                        end
+                        debugLog("Tower Cache:" .. json.encode(TowerCache))
+                    end)
                 end)
             end)
-        end)
 
-        RegisterNetEvent("SonoranCAD::sonrad:SetDishStatus")
-        AddEventHandler("SonoranCAD::sonrad:SetDishStatus", function(towerId, dishStatus)
-            local tower = GetTowerFromId(towerId)
-            if not tower then return end
-            tower.DishStatus = dishStatus
-            local pct = GetTowerCapacity(tower)
-            local color = nil
-            local status = nil
-            if pct == 1 then
-                -- Tower is alive and well.
-                debugLog("TOWER IS HEALTHY")
-                color = "#00a6ff"
-                status = "HEALTHY"
-            elseif pct == 0 then
-                -- Tower is offline
-                debugLog("TOWER IS OFFLINE")
-                color = "#ff0000"
-                status = "OFFLINE"
-            else
-                -- Tower is degraded
-                debugLog("TOWER IS DEGRADED")
-                color = "#ff8c00"
-                status = "DEGRADED"
-            end
+            RegisterNetEvent("SonoranCAD::sonrad:SetDishStatus")
+            AddEventHandler("SonoranCAD::sonrad:SetDishStatus", function(towerId, dishStatus)
+                local tower = GetTowerFromId(towerId)
+                if not tower then return end
+                tower.DishStatus = dishStatus
+                local pct = GetTowerCapacity(tower)
+                local color = nil
+                local status = nil
+                if pct == 1 then
+                    -- Tower is alive and well.
+                    debugLog("TOWER IS HEALTHY")
+                    color = "#00a6ff"
+                    status = "HEALTHY"
+                elseif pct == 0 then
+                    -- Tower is offline
+                    debugLog("TOWER IS OFFLINE")
+                    color = "#ff0000"
+                    status = "OFFLINE"
+                else
+                    -- Tower is degraded
+                    debugLog("TOWER IS DEGRADED")
+                    color = "#ff8c00"
+                    status = "DEGRADED"
+                end
 
-            local data = {{
-                ["id"] = tower.BlipID,
-                ["subType"] = "repeater",
-                ["coordinates"] = {
-                    ["x"] = tower.PropPosition.x,
-                    ["y"] = tower.PropPosition.y
-                },
-                ["radius"] = tower.Range * 0.7937,
-                ["icon"] = "https://sonoransoftware.com/assets/images/icons/email/radio.png",
-                ["color"] = color,
-                ["tooltip"] =  "Radio Tower",
-                ["data"] = {
-                    {
-                        ["title"] = "Health",
-                        ["text"] = status,
+                local data = {{
+                    ["id"] = tower.BlipID,
+                    ["subType"] = "repeater",
+                    ["coordinates"] = {
+                        ["x"] = tower.PropPosition.x,
+                        ["y"] = tower.PropPosition.y
+                    },
+                    ["radius"] = tower.Range * 0.7937,
+                    ["icon"] = "https://sonoransoftware.com/assets/images/icons/email/radio.png",
+                    ["color"] = color,
+                    ["tooltip"] =  "Radio Tower",
+                    ["data"] = {
+                        {
+                            ["title"] = "Health",
+                            ["text"] = status,
+                        }
                     }
-                }
-            }}
-            BlipMan.modifyBlips(data, function(res)
-                debugLog(res)
+                }}
+                BlipMan.modifyBlips(data, function(res)
+                    debugLog(res)
+                end)
             end)
-        end)
+        else
+            debugLog("Disabling blip management, API version too low.")
+        end
         
 
         CreateThread(function()
